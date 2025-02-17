ddec227b4768b9a2c7c575a50b5a6ba5c64381fc
suecoalto <suecoalto@gmail.com>
2025-02-17 23:13:14 +0100

    SupaBaseDB
    Created a supabase account and initialized a new Supabase client using the create_client() method.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
 
+load_dotenv()
 
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+#initialize Supabase client
+supabase: Client = create_client(url, key)
+
+#Remove this code block 
+try:
+    response = supabase.auth.get_user()
+    print("Supabase connection is working!")
+    print(response)
+except Exception as e:
+    print("Supabase connection failed:", e)
+    
